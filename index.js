d65ecbeac443eb3a8682b6fce6d1e7497b918e4e
ffok123 <30338770+ffok123@users.noreply.github.com>
2024-02-28 10:47:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const {WebhookClient} = require('dialogflow-fulfillment');
+"postgres://iyuaawwnbtjugr:e99d7d5bb9a86ca7aed0aee1a9e86ff8f907a00d64990b7daf6327b84ac6dcfe@ec2-34-232-191-133.compute-1.amazonaws.com:5432/ddlac84kto2lmu"
+
+
+
 
 const app = express()
 app.use(bodyParser.json())
 const port = process.env.PORT || 3000
-
 
 app.post('/dialogflow-fulfillment', (request, response) => {
 
@@ -23,25 +26,46 @@ const dialogflowFulfillment = (request, response) => {
 
     const agent = new WebhookClient({request, response});
 
-    function sayHello(agent) {
-        agent.add("Hi, 110")
-    }
-    function findLoc(agent) {
 
-        
-    if(agent.parameters.locationHK=="Central" && agent.parameters.cuisine=="Japanese") {  
-        agent.add("Location is: " + agent.parameters.locationHK)
+    function sayHello(agent) {
+        agent.add("Hi, Welcome student")
+    }
+    function findLoc(agent) {   
+
+        /*console.log(agent.parameters.locationHK)
+        console.log(agent.parameters.cuisine)       
+        const client = new Client({connectionString: connectionString})
+        client.connect()
+
+        client.query("select * from restinfo where locationhk = '"+ agent.parameters.locationhk +"'and cuisine = '"+ agent.parameters.cuisine +"'limit 1", (err,res) =>{
+            //console.log(err,res) 
+            for (let row of res.rows) {
+                console.log(row);
+            }
+             
+            client.end()
+        })*/
+
+    if(agent.parameters.locationhk=="Central" && agent.parameters.cuisine=="Japanese") {  
+        agent.add("Location is: " + agent.parameters.locationhk)
         agent.add("Chosen cuisine is: " +agent.parameters.cuisine)
         agent.add("Suggested restaurant: Tokyo Milk Cheese Factory")
         agent.add("Address:Shop 4-6, G/F, Western Market, 323 Des Voeux Road Central")
         agent.add("Price:$51-$100") 
         agent.add("Do you like this restaurant (Yes/No)?") }
-    else if (agent.parameters.locationHK=="Tsing Yi" && agent.parameters.cuisine=="Korean") {  
-        agent.add("Location is: " + agent.parameters.locationHK)
+    else if (agent.parameters.locationhk=="Tsing Yi" && agent.parameters.cuisine=="Korean") {  
+        agent.add("Location is: " + agent.parameters.locationhk)
         agent.add("Chosen cuisine is: " +agent.parameters.cuisine)
         agent.add("Suggested restaurant: MeokBang Korean BBQ & BAR")
         agent.add("Address:Shop 4-6, G/F,Shop 119B, 1/F, Maritime Square 1, 33 Tsing King Road, Tsing Yi")
-        agent.add("Price:$100-$$200") 
+        agent.add("Price:$100-$200") 
+        agent.add("Do you like this restaurant (Yes/No?)") }
+    else if (agent.parameters.locationhk=="Sha Tin" && agent.parameters.cuisine=="Japanese") {  
+        agent.add("Location is: " + agent.parameters.locationhk)
+        agent.add("Chosen cuisine is: " +agent.parameters.cuisine)
+        agent.add("Suggested restaurant: Mi-Ne Sushi")
+        agent.add("3/F, New Town Plaza Phase 3, 18 Sha Tin Centre Street, Sha Tin")
+        agent.add("Price:$100-$200") 
         agent.add("Do you like this restaurant (Yes/No?)") }
     else {
         agent.add("No restaurant is suggested")
@@ -51,13 +75,33 @@ const dialogflowFulfillment = (request, response) => {
 
     }
     function findRestHK(agent) {
-        agent.add("Please enter another cusine")
+        //agent.add("Please enter another cusine")
+        if(agent.parameters.locationhk=="Central" && agent.parameters.cuisine=="Japanese") {  
+         
+            agent.add("Another suggested restaurant: S2 Kitchen")
+            agent.add("Address:Shop A, G/F, 51 Wellington Street, Central")
+            agent.add("Price:$400-$800") 
+            agent.add("Do you like this restaurant (Yes/No)?") }
+        else if (agent.parameters.locationhk=="Tsing Yi" && agent.parameters.cuisine=="Korean") {  
+   
+            agent.add("Another suggested restaurant: NeNe Chicken")
+            agent.add("Address:Shop 226, 2/F, Maritime Square 2, 33 Tsing King Road, Tsing Yi")
+            agent.add("Price:$100-$200") 
+            agent.add("Do you like this restaurant (Yes/No?)") }
+        else if (agent.parameters.locationhk=="Sha Tin" && agent.parameters.cuisine=="Japanese") {  
+
+            agent.add("Another suggested restaurant: Tonkichi Tonkatsu Seafood")
+            agent.add("Shop 707, 7/F, New Town Plaza Phase 1, 18 Sha Tin")
+            agent.add("Price:$200-400") 
+            agent.add("Do you like this restaurant (Yes/No?)") }
+
+
     }
 
     let intentMap = new Map();
     intentMap.set("Default Welcome Intent", sayHello)
     intentMap.set("locationEnquiry", findLoc)
-    intentMap.set("locationEnquiry - no", findRestHK)
+    intentMap.set("locationEnquiry-no", findRestHK)
     agent.handleRequest(intentMap);
     
 
